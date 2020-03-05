83ec2bf73bf86864f14702a694b30241a9d7fa06
Jacob Halsey <jacob@jhalsey.com>
2020-03-05 17:14:37 +0000

    stage 1a

--- core/bindings.h
+++ core/bindings.h
@@ -2,8 +2,13 @@
 
 #include   "GIC.h"
 #include "PL011.h"
+#include "SP804.h"
 
 // Include functionality relating to the   kernel.
 
 #include "lolevel.h"
 #include     "int.h"
+
+// Include Newlib
+
+//#include "stdlib.h"
--- hilevel/src/lib.rs
+++ hilevel/src/lib.rs
@@ -9,6 +9,9 @@ mod bindings;
 use core::panic::PanicInfo;
 use bindings::PL011_putc;
 use bindings::UART0;
+use bindings::TIMER0;
+use bindings::GICC0;
+use bindings::GICD0;
 use arr_macro::arr;
 use core::char::from_digit;
 use crate::ProcessStatus::{Executing, Ready};
@@ -47,6 +50,8 @@ static mut PROCESS_TABLE: [Option<ProcessControlBlock>; 64] = arr![None; 64];
 extern {
     fn main_P1();
     fn main_P2();
+    fn main_P3();
+    fn main_P4();
     static tos_P1: u32;
     static tos_P2: u32;
 }
@@ -118,7 +123,7 @@ pub extern fn hilevel_handler_rst(ctx: *mut Context) {
             top_of_stack: tos,
             context: Context {
                 cpsr: CPSR_USR,
-                pc: main_P1 as u32,
+                pc: main_P3 as u32,
                 gpr: [0; 13],
                 sp: tos,
                 lr: 0,
@@ -133,7 +138,7 @@ pub extern fn hilevel_handler_rst(ctx: *mut Context) {
             top_of_stack: tos,
             context: Context {
                 cpsr: CPSR_USR,
-                pc: main_P2 as u32,
+                pc: main_P4 as u32,
                 gpr: [0; 13],
                 sp: tos,
                 lr: 0,
@@ -142,12 +147,42 @@ pub extern fn hilevel_handler_rst(ctx: *mut Context) {
     }
 
     unsafe {
+        (*TIMER0).Timer1Load  = 0x00100000; // select period = 2^20 ticks ~= 1 sec
+        (*TIMER0).Timer1Ctrl  = 0x00000002; // select 32-bit   timer
+        (*TIMER0).Timer1Ctrl |= 0x00000040; // select periodic timer
+        (*TIMER0).Timer1Ctrl |= 0x00000020; // enable          timer interrupt
+        (*TIMER0).Timer1Ctrl |= 0x00000080; // enable          timer
+
+        (*GICC0).PMR          = 0x000000F0; // unmask all            interrupts
+        (*GICD0).ISENABLER1  |= 0x00000010; // enable timer          interrupt
+        (*GICC0).CTLR         = 0x00000001; // enable GIC interface
+        (*GICD0).CTLR         = 0x00000001; // enable GIC distributor
+
+        bindings::int_enable_irq();
+    }
+
+    unsafe {
         dispatch(ctx, &mut None, &mut PROCESS_TABLE[0]);
     }
 }
 
 #[no_mangle]
-pub extern fn hilevel_handler_irq() {
+pub extern fn hilevel_handler_irq(ctx: *mut Context) {
+    let ctx = unsafe { &mut *ctx};
+
+    unsafe {
+        let id: u32 = (*GICC0).IAR;
+
+        if id == bindings::GIC_SOURCE_TIMER0 {
+
+            PL011_putc(UART0, 'T' as u8, true);
+            (*TIMER0).Timer1IntClr = 0x01;
+            schedule(ctx);
+        }
+
+        (*GICC0).EOIR = id;
+    }
+
 }
 
 #[no_mangle]
