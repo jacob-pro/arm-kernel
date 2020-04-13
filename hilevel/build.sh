e20ebf73a31456b04a4ae26c1c63f19f025b4bad
Jacob Halsey <jacob@jhalsey.com>
2020-04-13 18:26:37 +0100

    fix build

--- hilevel/build.sh
+++ hilevel/build.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-set -x #echo on
 
-if ! [ -x "$(command -v rustup)" ]; then
+if ! command -v rustup &>/dev/null; then
   echo 'Downloading rustup...'
   curl https://sh.rustup.rs -sSf | sh -s -- -y
   source $HOME/.cargo/env
 fi
 
-if ! [ -x "$(command -v clang)" ]; then
+if ! command -v clang &>/dev/null; then
   echo 'Error: please make sure clang is in your path'
   exit 1
 fi
 
 # Sometimes clang-sys detects llvm-private instead which is not compatible
-if [ -x "$(command -v yum)" ] && [ -x "$(yum list installed llvm-private)" ]; then
+if command -v yum &>/dev/null && yum list installed llvm-private &>/dev/null; then
   echo 'Warning: Detected yum package llvm-private. Attempting alternative llvm path...'
   export LIBCLANG_PATH=$(llvm-config --libdir)
 fi
+
+set -x #echo on
 
 # armv7a currently only available in Rust 1.42
 # https://github.com/rust-lang/rust/pull/68253
